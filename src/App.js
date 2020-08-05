fe651276ff62f990ea53d970b9027ac78b8326ea
rodrok7 <rodrok7@gmail.com>
2020-08-04 20:08:47 -0500

    first commit :octocat:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Greet from "./components/Greet";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <Greet />
       </header>
     </div>
   );
---
+++ src/components/Greet/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Greet() {
+  return <p>Hola Mundo</p>;
+}
+
+export default Greet;
